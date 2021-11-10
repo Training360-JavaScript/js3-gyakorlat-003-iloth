de2a89c1a5c464f6e5818f10e3a253e59a979e81
iloth <loth.istvan@hotmail.com>
2021-11-10 22:49:54 +0100

    Solution

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,1 +1,8 @@
+const setCookie = (string = '') => {
+  const exp = new Date();
+  exp.setTime(exp.getTime() + (15 * 60 * 1000));
 
+  document.cookie = `token=${string};expires=${exp.toUTCString()}`;
+};
+
+export default setCookie;
